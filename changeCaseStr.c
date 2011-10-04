9e9a8e8e345e23eff8c9daab04f4fe57b8fa3fbe
sjbarag <sjbarag@gmail.com>
2011-10-04 08:22:51 -0400

    Add fork to parent & child
    They don't communicate yet

--- changeCaseStr.c
+++ changeCaseStr.c
@@ -1,34 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_STRING_SIZE 256
 
 int main(int argc, char **argv)
 {
+	/* check for correct number of arguments.
+	 *   no need to declare anything if we're not going to run!
+	 */
 	if(argc == 1)
 	{
 		printf("USAGE: $ changeCaseStr text\n");
 		return -1;
 	}
 
-	char data[MAX_STRING_SIZE];
-	char *str = data;
+	char data[MAX_STRING_SIZE];   // array in which to store the string from argv[1]
+	char *str = data;             // pointer to data
+	int pid;                      // process ids!
 
 	str = argv[1];
 
-	for( int i = 0; i < MAX_STRING_SIZE; i++)
+	if( (pid = fork()) < 0) /* fork! */
 	{
-		if( str[i] == '\0' )
-			break;
-		if (str[i] >= 'a' && str[i] <= 'z')
-			printf("%c", toupper(str[i]) );
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			printf("%c", tolower(str[i]) );
-		else
-			printf("%c", str[i] );
+		printf("OH NOEZ!  Fork error.\n");
+		exit(-1);
 	}
-	printf("\n");
+	else if(pid == 0)       /* if child */
+	{
+		printf("Child here!\n");
+		for( int i = 0; i < MAX_STRING_SIZE; i++)
+		{
+			if( str[i] == '\0' )
+				break;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				printf("%c", toupper(str[i]) );
+			else if (str[i] >= 'A' && str[i] <= 'Z')
+				printf("%c", tolower(str[i]) );
+			else
+				printf("%c", str[i] );
+		}
+		printf("\n");
+		exit(0);
+	}
+
+	printf("Parent here!\n");
+
 	return 0;
 }
 
