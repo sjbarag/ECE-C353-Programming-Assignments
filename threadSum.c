f2fdabadcdeeed26dc4ee14a20119da8b5119bec
sjbarag <sjbarag@gmail.com>
2011-10-09 10:10:40 -0400

    Generate random numbers
    Incorrectly sums stage one.
    Don't forget: threads are non-deterministic, so you can't reuse the same
    storage.  A cascading sum occurs, hence the extremely large sums compared to
    the actual sum.

--- threadSum.c
+++ threadSum.c
@@ -3,8 +3,9 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define NUM_GENERATED  5000000 // total number of randomly generated floats
+#define NUM_GENERATED  500 // total number of randomly generated floats
 #define NUM_THREADS_S1      25 // number of threads in stage one
 #define NUM_THREADS_S2       5 // number of threads in stage two
 
@@ -30,24 +31,48 @@ typedef struct thread_args
 
 int main()
 {
+	float long_sum = 0;
+
+	/* seed random number generator */
+	srandom(time(NULL));
+
+	/* generate 5000000 floats and calculate sum */
+	printf("Generating random numbers between 0 and 1\n");
+	for(int i = 0; i < NUM_GENERATED; i++)
+	{
+		data[i] = (float)random()/(float)RAND_MAX;
+		long_sum += data[i];
+	}
+
+
 	pthread_t main_thread;
 	pthread_t s1_thread[NUM_THREADS_S1]; // array of stage one threads
 	pthread_t s2_thread[NUM_THREADS_S2]; // array of stage two threads
-	THREAD_ARGS *t_args;
+	THREAD_ARGS *t_args;                 // structure to hold the arguments for
+	                                     // each thread
+	float t_sum = 0;                     // final sum via threads
+
+	float dbg_sum = 0;
 
 	main_thread = pthread_self();  // get thread ID for calling thread
 	printf("Main thread = %u is creating %d worker threads \n", (int)main_thread, NUM_THREADS_S1);
 
-	printf("NUM_TO_SUM_S1 = %i\n", NUM_TO_SUM_S1);
 
 	/* -------------------- stage one -------------------- */
+	printf("First 20:\n[ ");
+	for(int i = 0; i < 20; i++)
+	{
+		printf("%f, ", data[i]);
+		dbg_sum += data[i];
+	}
+	printf(" ]\n");
+	printf("First 20 sum = %f", dbg_sum);
 
 	printf("----- Creating threads -----\n");
 	/* create threads for stage one */
 	for(int i = 0; i < NUM_THREADS_S1; i++)
 	{
 
-		printf("i = %i\n", i);
 		/* allocate memory for the struct that contains each thread's args */
 		t_args = (THREAD_ARGS *)malloc(sizeof(THREAD_ARGS));
 
@@ -78,9 +103,15 @@ int main()
 
 void *partial_sum(void *in_args)
 {
+	float temp_sum = 0.0;
 	THREAD_ARGS *l_args = (THREAD_ARGS *)in_args;
 
 	/* print args */
-	printf("Thread %i is using:\n  ID =  \t%i \n  start =\t%i \n  num =  \t%i\n",
-		l_args->threadID, l_args->start_pos, l_args->num_to_sum);
+	//printf("Thread %i is using:\n  ID =  \t%i \n  start =\t%i \n  num =  \t%i\n",
+	//	l_args->threadID, l_args->threadID, l_args->start_pos, l_args->num_to_sum);
+
+	for(int i = l_args->start_pos; i < (l_args->start_pos + l_args->num_to_sum); i++)
+		temp_sum = (float)temp_sum + (float)data[i];
+	printf("ThreadID: %i\ntemp_sum = %f\nnum_summed = %i\n\n", l_args->threadID, temp_sum, l_args->num_to_sum);
+	data[l_args->threadID] = (float)temp_sum;
 }
