b6b9c065856ed2f1ef5c98ef5bc6e0c1d58346c0
sjbarag <sjbarag@gmail.com>
2011-10-16 19:43:10 -0400

    Add comments and cleaning

--- matrixMult.c
+++ matrixMult.c
@@ -9,7 +9,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-
+/* structure for thread arguments */
 typedef struct thread_args
 {
 	int i;
@@ -19,86 +19,77 @@ typedef struct thread_args
 	int **dst;
 } THREAD_ARGS;
 
-void *multValues(void *);
-int g_numRows;
-int *outArray;
+void *multValues(void *);  /* process to run for each thread */
+int g_numRows;             /* global version of number of rows */
+int *outArray;             /* pointer to where the final values will go */
 
 int main()
 {
-
-	FILE *input;
-	char lineData[64];
+	FILE *input;           /* file descriptor for the input file */
+	char lineData[64];     /* each line gets stored here */
 	char *line = lineData;
-	int numRows;
-	int numCols;
-//	char *line = (char *)malloc(sizeof(char)*64);
-	input = fopen("input_data.txt", "r");
-	fgets(line, 24, input);
-	line = strtok(line, "\n"); // strip newline
-	line = strtok(line, "ROWS ");
-	numRows = atoi(line);
-	g_numRows = numRows;
+	int numRows;           /* number of rows in the matrix, read from 'input' */
+	int numCols;           /* number of columns.  Equal to numCols, not read */
 
-	//fscanf(input, "ROWS %i", &numRows);
-	numCols = numRows;
-	fgets(line, 64, input); // columns
+	input = fopen("input_data.txt", "r");  /* open the input file */
+	fgets(line, 24, input);                /* get the first line */
+	line = strtok(line, "\n");             /* strip the newline */
+	line = strtok(line, "ROWS ");          /* isolate just the value */
+	numRows = atoi(line);                  /* convert to int, yo */
+	g_numRows = numRows;                   /* there's a global version too */
+
+	numCols = numRows;                     /* NxN matrix, so make them equal */
+	fgets(line, 64, input);                /* read the line for columns, but ignore it */
 
 
+	/* allocate two-dimensional array for matrix A */
 	int **A = (int **)malloc(numRows * sizeof(int *));
 	for(int i = 0; i < numRows; i ++)
 		A[i] = (int *)malloc(numCols * sizeof(int));
 
+	/* allocate two-dimensional array for matrix B */
 	int **B = (int **)malloc(numRows * sizeof(int *));
 	for(int i = 0; i < numRows; i ++)
 		B[i] = (int *)malloc(numCols * sizeof(int));
 
+	/* allocate two-dimensional array for output matrix */
 	int **out = (int **)malloc(numRows * sizeof(int *));
 	for(int i = 0; i < numRows; i ++)
 		out[i] = (int *)malloc(numCols * sizeof(int));
 
-	//int *p_data = data;
-	char *val;
+
 	fgets(line, 64, input); // handle blankline
 
 	pthread_t thread_pool[numRows][numCols];
 
 	THREAD_ARGS *t_args;
 
-	// do it once for A and B.  Too lazy to copy paste this, so I'll carry arround a 3-dimensional array...
-	for(int row=0; row < numRows; row++)
-	{
-		fgets(line, numRows*2+1, input);
-		for(int col=0; col < numCols; col++) {
-			if( col == 0)
-			{
-				val = strtok(line, " ");
-				A[row][col] = atoi(val);
-			}
-			else
-			{
-				val = strtok(NULL, " ");
-				A[row][col] = atoi(val);
-			}
-		}
-	}
-
-	fgets(line, numRows*2+1, input); // handle blankline
-
+	/* read data for matrix A elements */
 	for(int row=0; row < numRows; row++)
 	{
 		fgets(line, numRows*2+1, input);
 		for(int col=0; col < numCols; col++)
 		{
 			if( col == 0)
-			{
-				val = strtok(line, " ");
-				B[row][col] = atoi(val);
-			}
+				A[row][col] = atoi(strtok(line, " "));
 			else
-			{
-				val = strtok(NULL, " ");
-				B[row][col] = atoi(val);
-			}
+				A[row][col] = atoi(strtok(NULL, " "));
+		}
+	}
+
+	/* ignore blank line between matrices */
+	fgets(line, numRows*2+1, input);
+
+	/* read data for matrix B elements */
+	for(int row=0; row < numRows; row++)
+	{
+		fgets(line, numRows*2+1, input);
+		for(int col=0; col < numCols; col++)
+		{
+			if( col == 0)
+				B[row][col] = atoi(strtok(line, " "));
+			else
+				B[row][col] = atoi(strtok(NULL, " "));
 		}
 	}
 
@@ -108,14 +99,17 @@ int main()
 	{
 		for(int j = 0; j < numCols; j++)
 		{
+			/* allocate new args structure */
 			t_args = (THREAD_ARGS *)malloc(sizeof(THREAD_ARGS));
 
+			/* fill args */
 			t_args->i = i;
 			t_args->j = j;
 			t_args->srcA = A;
 			t_args->srcB = B;
 			t_args->dst = out;
 
+			/* create thread */
 			if( pthread_create( &thread_pool[i][j], NULL, multValues, (void *)t_args) != 0)
 			{
 				printf("Error: could not create thread! Exiting.\n");
@@ -124,10 +118,12 @@ int main()
 		}
 	}
 
+	/* wait for all threads to return */
 	for( int i = 0; i < numRows; i++)
 		for( int j = 0; j < numCols; j++)
 			pthread_join( thread_pool[i][j], NULL );
 
+	/* print final result */
 	printf("Product:\n");
 	for(int row = 0; row < numRows; row++)
 	{
@@ -139,12 +135,17 @@ int main()
 
 void *multValues(void *input_args)
 {
+	/* cast to usable datatype */
 	THREAD_ARGS *l_args = (THREAD_ARGS *)input_args;
 
-	// cast and save local args
+	/* these is just shorter to write */
 	int i = l_args->i;
 	int j = l_args->j;
 
+	/* calculate values */
 	for(int k = 0; k < g_numRows; k++)
 		l_args->dst[i][j] += l_args->srcA[i][k] * l_args->srcB[k][j];
+
+	/* peace out */
+	pthread_exit(0);
 }
