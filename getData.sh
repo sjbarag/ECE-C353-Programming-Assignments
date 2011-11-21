5ca89b824e3ae48184cbe094af7216dc54ef6e27
sjbarag <sjbarag@gmail.com>
2011-11-21 11:12:41 -0500

    Add data acquisition script

---
+++ getData.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Gets data for the ECE-C353 final project
+SIG_TEXT="Scriptable output:"
+> data.txt
+for loop in {1..10}
+do
+	for threads in 2 4 8 16
+	do
+		out/work_crew double ~/school $i | grep "$SIG_TEXT" | sed "s/$SIG_TEXT\t//" >> data.txt
+	done
+done
