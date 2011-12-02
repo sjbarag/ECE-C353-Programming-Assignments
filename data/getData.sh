d0fd3f75f0709c5a34fed18c06b29218a61c9008
sjbarag <sjbarag@gmail.com>
2011-12-01 20:12:09 -0500

    Put output in variable

--- data/getData.sh
+++ data/getData.sh
@@ -4,16 +4,17 @@ SIG_TEXT="Scriptable output:"
 STRING="kandasamy"
 DIR="/home/DREXEL/nk78"
 NUM_RUNS=10
+OUT="data.txt"
 
-> data.txt
+> $OUT
 for loop in {1..100}
 do
 	echo "Running... ($loop/$NUM_RUNS)"
 	for threads in 2 4 8 16
 	do
-		out/work_crew $STRING $DIR $threads | grep "$SIG_TEXT" | sed "s/$SIG_TEXT\t//" >> data2.txt
+		out/work_crew $STRING $DIR $threads | grep "$SIG_TEXT" | sed "s/$SIG_TEXT\t//" >> $OUT
 	done
 	# convenient line break
-	echo >> data2.txt
+	echo >> $OUT
 done
 echo "All done!"
