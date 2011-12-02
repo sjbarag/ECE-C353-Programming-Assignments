58c85945ee1d9027fc58fd20716fdc9dc9b52ba8
sjbarag <sjbarag@gmail.com>
2011-12-01 21:05:53 -0500

    Handle single-threaded data

--- data/calcData.py
+++ data/calcData.py
@@ -3,11 +3,22 @@ f = open('data_processed.txt', 'r')
 lines = []
 for line in f:
 	lines.append(line.strip().split('\t'))
+f.close()
 
 cols = []
 for i in range(4):
 	cols.append([row[i] for row in lines])
 
+f = open('data_100.txt', 'r')
+slines = []
+for line in f:
+	if line != '\n':
+		slines.append(line.strip().split('\t'))
+f.close()
+
+cols.insert(0, [row[1] for row in slines])
+
+
 print "#num threads\taverage\t\tmin\t\tmax"
-for i in range(4):
-	print 2**(i+1), "\t\t", sum(float(v) for v in lines[i])/len(lines[i]), "\t", min(lines[i]), "\t", max(lines[i])
+for i in range(len(cols)):
+	print 2**(i), "\t\t", sum(float(v) for v in cols[i])/len(cols[i]), "\t", min(cols[i]), "\t", max(cols[i])
